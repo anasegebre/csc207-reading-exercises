c82b06ee7f16a4e385f51d6d71168f9d2ee148e6
Ana Segebre <anasegebre@Anas-MacBook-Pro.local>
2017-02-08 12:18:58 -0600

    02-07 & 02-08

---
+++ 02-07/src/max.java
@@ -0,0 +1,32 @@
+
+public class max {
+
+	
+	/*
+	 * 1. Inputs: arr(n)
+	 * 2. Operations: comparison
+	 * 3. Best: 1, f ∈ O(1) It is constant
+	 *    Worst: n - 1,  f ∈ O(n-1) It is linear
+	 *    Average: n/a either one or the other
+	 */
+	public static int max(int[] arr) {
+		if (arr.length == 0) {
+			throw new IllegalArgumentException();
+		} else {
+			int ret = arr[0];
+			for (int i = 1; i < arr.length; i++) {
+				if (ret < arr[i]) {
+					ret = arr[i];
+				} 
+			}
+			return ret; 
+		}
+	}
+	
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
